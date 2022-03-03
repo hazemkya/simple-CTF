9f2b8ecd6b266bca128db3ade8cd84dec99ead6e
Hazem <hazemkya@gmail.com>
2022-03-03 16:18:48 +0300

    first commit

---
+++ make_ctf.py
@@ -0,0 +1,16 @@
+import secrets
+import string
+from datetime import datetime
+
+alphabet = string.ascii_letters + string.digits
+flag1 = 'Flag{'+''.join(secrets.choice(alphabet) for i in range(24)) + '}'
+flag2 = 'Flag{'+''.join(secrets.choice(alphabet) for i in range(24)) + '}'
+
+date = datetime.now().strftime("%Y_%m_%d-%H_%M")
+print()
+
+
+with open(f"keys_{date}.txt", "w") as file:
+    # Writing data to a file
+    file.write('Flag1: '+flag1 + '\n')
+    file.write('Flag2: '+flag2 + '\n')
