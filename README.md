5896d73340a113595cd84c35c6094349e071502a
Hazem <hazemkya@gmail.com>
2022-04-17 19:43:05 +0300

    Done

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# make-ctf
-Make a simple CTF with unique flags with a single line
+# simple-CTF
+
+This is a simple CTF challenge, you can take this app and deploy it on your favorite hosting platform, it is not recommended to self-host the app since it's not been tested for safe use.
+
+   
--- generate_keys.py
+++ generate_keys.py
@@ -5,12 +5,13 @@ from datetime import datetime
 alphabet = string.ascii_letters + string.digits
 flag1 = 'Flag{'+''.join(secrets.choice(alphabet) for i in range(24)) + '}'
 flag2 = 'Flag{'+''.join(secrets.choice(alphabet) for i in range(24)) + '}'
+flag3 = 'Flag{'+''.join(secrets.choice(alphabet) for i in range(24)) + '}'
 
 date = datetime.now().strftime("%Y_%m_%d-%H_%M")
-print()
 
 
 with open(f"keys_{date}.txt", "w") as file:
     # Writing data to a file
     file.write('Flag1: '+flag1 + '\n')
     file.write('Flag2: '+flag2 + '\n')
+    file.write('Flag3: '+flag3 + '\n')
